ec2fe53f69407f21eab1cb99c7aa860e0d7add29
wangtongming <1689732695@qq.com>
2024-12-27 18:42:21 +0800

    feat:更新组件

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from "vue";
 import Mostify from "@/mostify/components";
 import App from "./App.vue";
 import router from "./router";
-import '../lib/mostify-ui/dist/dark.css'
-
 // document.documentElement.classList.remove("dark");
 // document.documentElement.classList.add("dark");
 const app = createApp(App);
